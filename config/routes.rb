85d0c49043a73a442fa3b2c738cea156c7369702
Sudhir Jonathan <sudhir.j@gmail.com>
2019-03-23 01:04:12 +0530

    Started adding stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/:slug', to: 'companies#show'
   root to: 'home#index'
 end
