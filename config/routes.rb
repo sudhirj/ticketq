92622ea4ec7083f5e1789647483064b5484ffef4
Sudhir Jonathan <sudhir.j@gmail.com>
2019-03-23 19:09:39 +0530

    Added performance page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/:company_slug/:show_slug/:datetime', to: 'performances#show'
   get '/:company_slug/:show_slug', to: 'shows#show'
   get '/:company_slug', to: 'companies#show'
   root to: 'home#index'
