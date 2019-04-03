c7c58cd9c52a4133786d2e81ad76d8e621999d34
Sudhir Jonathan <sudhir.j@gmail.com>
2019-04-03 23:06:25 +0530

    Added reasonable testing seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+museum = Venue.create_or_find_by! slug: 'museum-theatre'
+museum.update_attributes! name: 'Museum Theatre', area: 'Egmore'
+academy = Venue.create_or_find_by! slug: 'music-academy'
+academy.update_attributes! name: 'Music Academy', area: 'TTK Road'
+
+kuku = Company.create_or_find_by! slug: 'the-kuku-company'
+kuku.update_attributes! name: 'The Kuku Company'
+
+company = kuku.shows.create_or_find_by! slug: 'company'
+company.update_attributes! name: 'Company', tagline: 'A Musical Comedy'
+lesmis = kuku.shows.create_or_find_by! slug: 'lesmis'
+lesmis.update_attributes! name: 'Lés Miserables', tagline: 'The GLUMS!'
+
+Performance.delete_all
+company.performances.create! venue: museum, showtime: rand(100..10000).minutes.from_now
+company.performances.create! venue: academy, showtime: rand(100..10000).minutes.from_now
+company.performances.create! venue: museum, showtime: rand(100..10000).minutes.from_now
+company.performances.create! venue: museum, showtime: rand(100..10000).minutes.from_now
+lesmis.performances.create! venue: academy, showtime: rand(100..10000).minutes.from_now
+lesmis.performances.create! venue: academy, showtime: rand(100..10000).minutes.from_now
+lesmis.performances.create! venue: academy, showtime: rand(100..10000).minutes.from_now
+lesmis.performances.create! venue: academy, showtime: rand(100..10000).minutes.from_now
+lesmis.performances.create! venue: academy, showtime: rand(100..10000).minutes.from_now
+
