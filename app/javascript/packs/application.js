b74784fd9fe3ed2965a6ed594ae182439fb779c5
Sudhir Jonathan <sudhir.j@gmail.com>
2019-04-15 17:11:12 +0530

    Removed extra JS libs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,5 +5,5 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("@rails/activestorage").start()
+// require("channels")
