123854a0b4ebb4a6e0e1fadecd0c75b45ebfcc29
Sudhir Jonathan <sudhir.j@gmail.com>
2019-04-20 00:27:31 +0530

    Added description, terms

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -2,7 +2,9 @@ class ShowsController < ApplicationController
   def show
     @company = Company.find_by! slug: params[:company_slug]
     @show = @company.shows.find_by! slug: params[:show_slug]
-    @performances = Performance.includes(:venue).where(show: @show).to_a
+    @performances = Performance.includes(:venue).where(show: @show)
+    @performances = @performances.where(venue: Venue.find_by!(slug: params[:venue_slug])) if params[:venue_slug].present?
+    @performances = @performances.to_a
     @venue_groups = @performances.group_by(&:venue)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -15,7 +17,40 @@ kuku = Company.create_or_find_by! slug: 'the-kuku-company'
 kuku.update_attributes! name: 'The Kuku Company', rp_account: 'acc_7oRhJRK7HO4IDJ'
 
 company = kuku.shows.create_or_find_by! slug: 'company'
-company.update_attributes! name: 'Company', tagline: 'A Musical Comedy'
+company.update_attributes! name: 'COMPANY', tagline: 'A Musical Comedy'
+company.update_attributes!(
+  cast: [
+    { name: 'Sandeep John' },
+    { name: 'Dr Yohan Chacko' },
+    { name: 'Pallavi V' },
+    { name: 'Navarre Roy' },
+    { name: 'Kavya Joseph' },
+    { name: 'Freddy Koikaran' },
+    { name: 'Kamatchi' },
+    { name: 'Gayathri Pradeep' },
+    { name: 'Ananth Bhaskararaman' },
+    { name: 'Deepa Nambiar' },
+    { name: 'Denver Anthony Nicholas' },
+    { name: 'Aishvarrya' },
+    { name: 'Sangita Santosham' },
+    { name: 'Amrita Fredrick' }
+  ],
+  crew: [
+    { name: 'Amrita Fredrick', role: 'Director' },
+    { name: 'Sangita Santosham', role: 'Vocal Director' },
+    { name: 'Hannah Elliot', role: 'Vocal Coach' },
+    { name: 'Srikanth Gananasekaran', role: 'Pianist' }
+  ],
+  runtime: { hours: 2, minutes: 15, intermission: true },
+  about: [
+    'COMPANY follows Bobby, a commitment phobic 35 year old through a musical dissection of love and relationships. Aided by the advice of his 5 married couple friends, Bobby juggles the pros and cons of marriage with endearing scepticism that will have audiences laughing and nodding in recognition.',
+    'There will be a Q&A session with the cast and musicians on SATURDAY & SUNDAY after the show.'
+  ],
+  terms: [
+    'Please pick up tickets from the box-office 30 minutes before showtime.',
+    'Once tickets are purchased there will be no refunds or cancellations.'
+  ]
+)
 lesmis = kuku.shows.create_or_find_by! slug: 'lesmis'
 lesmis.update_attributes! name: 'Lés Miserables', tagline: 'The GLUMS!'
 
@@ -25,15 +60,15 @@ Denomination.delete_all
 Performance.delete_all
 
 perfs = []
-perfs << company.performances.create!(venue: museum, showtime: rand(100..10000).minutes.from_now)
-perfs << company.performances.create!(venue: academy, showtime: rand(100..10000).minutes.from_now)
-perfs << company.performances.create!(venue: museum, showtime: rand(100..10000).minutes.from_now)
-perfs << company.performances.create!(venue: museum, showtime: rand(100..10000).minutes.from_now)
-perfs << lesmis.performances.create!(venue: academy, showtime: rand(100..10000).minutes.from_now)
-perfs << lesmis.performances.create!(venue: academy, showtime: rand(100..10000).minutes.from_now)
-perfs << lesmis.performances.create!(venue: academy, showtime: rand(100..10000).minutes.from_now)
-perfs << lesmis.performances.create!(venue: academy, showtime: rand(100..10000).minutes.from_now)
-perfs << lesmis.performances.create!(venue: academy, showtime: rand(100..10000).minutes.from_now)
+perfs << company.performances.create!(venue: museum, showtime: DateTime.parse('2019-06-07T19:30:00+05:30'))
+perfs << company.performances.create!(venue: museum, showtime: DateTime.parse('2019-06-08T18:30:00+05:30'))
+perfs << company.performances.create!(venue: museum, showtime: DateTime.parse('2019-06-09T18:30:00+05:30'))
+
+perfs << lesmis.performances.create!(venue: academy, showtime: rand(100..10_000).minutes.from_now)
+perfs << lesmis.performances.create!(venue: academy, showtime: rand(100..10_000).minutes.from_now)
+perfs << lesmis.performances.create!(venue: academy, showtime: rand(100..10_000).minutes.from_now)
+perfs << lesmis.performances.create!(venue: academy, showtime: rand(100..10_000).minutes.from_now)
+perfs << lesmis.performances.create!(venue: academy, showtime: rand(100..10_000).minutes.from_now)
 
 perfs.each do |perf|
   denoms = []
