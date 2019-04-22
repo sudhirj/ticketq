8f6e041145e723b5e834097f788112cae2bbfe9e
Sudhir Jonathan <sudhir.j@gmail.com>
2019-04-22 18:00:21 +0530

    Cleaned up show lists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def responsive_image_tag path, opts, tag_opts
+    "<img alt='#{tag_opts[:alt]}' style='#{tag_opts[:style]}' srcset='#{[1, 1.5, 2, 3].map {|x| ix_image_url(path, opts.merge({dpr: x})) + " #{x}x"}.join(', ') }' src='#{ix_image_url(path, {h: 50, fit: 'clip'})}'/>".html_safe
+  end
 end
