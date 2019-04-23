5a84060ce9ab7813d685730bb6d4a4c1b9dcc8c7
Sudhir Jonathan <sudhir.j@gmail.com>
2019-04-23 23:51:54 +0530

    Showing shows on the homepage.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @performances = Performance.includes(%i[show venue]).joins(show: [:company]).to_a
+    @performance_groups = @performances.group_by { |p| OpenStruct.new(show: p.show, venue:  p.venue) }
   end
 end
