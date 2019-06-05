6d94d8ee939f967c84525060c06ecfc30a762504
Sudhir Jonathan <sudhir.j@gmail.com>
2019-06-05 23:31:23 +0530

    Added overall report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,37 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
+  def index
+    response.headers['Content-Type'] ||= 'text/csv'
+    response.headers['Content-Disposition'] = "attachment; filename=#{['all', DateTime.current].join(' ').parameterize}.csv"
+    response.headers['Content-Transfer-Encoding'] = 'binary'
+    response.headers['Last-Modified'] = Time.current.ctime.to_s
+
+    self.response_body = Enumerator.new do |yielder|
+      yielder << [
+        'Show Time',
+        'Show Name',
+        'Denomination',
+        'Allocated',
+        'Sold',
+        'Online Occupancy',
+        'Revenue'
+      ].to_csv
+      Denomination.all.each do |denom|
+        yielder << [
+          denom.performance.showtime.to_s,
+          denom.show.name,
+          [denom.name, denom.price].join(' / '),
+          denom.allocated_count,
+          denom.confirmed_count,
+          ((denom.confirmed_count.to_f * 100) / denom.allocated_count.to_f).round.to_s + '%',
+          denom.confirmed_count * denom.price
+        ].to_csv
+      end
+    end
+  end
+
+
   def show
     performance = Performance.find(params[:id])
 
