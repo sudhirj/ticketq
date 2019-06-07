d94a26ca229cd7f7319bacac913f5afda47fd5e9
Sudhir Jonathan <sudhir.j@gmail.com>
2019-06-08 01:24:00 +0530

    Don't show finished shows

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -4,6 +4,7 @@ class ShowsController < ApplicationController
     @show = @company.shows.find_by! slug: params[:show_slug]
     @performances = Performance.includes(:venue).where(show: @show)
     @performances = @performances.where(venue: Venue.find_by!(slug: params[:venue_slug])) if params[:venue_slug].present?
+    @performances = @performances.where('showtime > ?', DateTime.current + 2.hours)
     @performances = @performances.to_a
     @venue_groups = @performances.group_by(&:venue)
   end
